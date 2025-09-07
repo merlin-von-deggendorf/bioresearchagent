e0c00ad97377f72c3fd649a63cf810eea99f7747
David <david.maruhn@stud.th-deg.de>
2025-09-07 15:03:21 +0200

    Add initial implementation of Gwasapi class and main entry point

---
+++ gwascat/gwasapi.py
@@ -0,0 +1,5 @@
+class Gwasapi:
+    def __init__(self):
+        pass
+    def print_message(self):
+        print("Hello from Gwasapi")
---
+++ main.py
@@ -0,0 +1,5 @@
+from gwascat import Gwasapi
+
+if __name__ == "__main__":
+    api = Gwasapi()
+    api.print_message()
